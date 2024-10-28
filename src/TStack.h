f01b6593470991c2111d15dfc996b970778a199e
prostatus <mel.sirvania@gmail.com>
2024-10-28 11:25:22 +0300

    TStack implemented

---
+++ src/TStack.h
@@ -0,0 +1,65 @@
+#pragma once
+
+template<class T>
+class TStack {
+	T* pMem;
+	int maxSize;
+	int index;
+public:
+	TStack(int maxSize = 8) {
+		if (maxSize > 0) {
+			pMem = new T[maxSize];
+			this->maxSize = maxSize;
+			index = -1;
+		}
+		else { throw 2531; }
+	}
+	TStack(const TStack<T>& s) {
+		pMem = new T[s.maxSize];
+		maxSize = s.maxSize;
+		index = s.index;
+	}
+	~TStack() { delete[] pMem; }
+
+
+	TStack<T>& operator=(const TStack<T>& s) {
+		if (this == &s) return *this;
+		if (maxSize != s.maxSize) {
+			T* tmp = new T[s.maxSize];
+			delete[] pMem;
+			pMem = tmp;
+			maxSize = s.maxSize;
+		}
+		index = s.index;
+		for (int i = 0; i < index; i++) pMem[i] = s.pMem[i];
+		return *this;
+	}
+	bool operator==(const TStack<T>& s) {
+		if (this == &s) return true;
+		if ((maxSize != s.maxSize) || (index != s.index)) return false;
+		for (int i = 0; i < index; i++) {
+			if (pMem[i] != s.pMem[i]) return false;
+		}
+		return true;
+	}
+	bool operator!=(const TStack<T>& s) {
+		return !(this->operator==(s));
+	}
+
+
+	bool isEmpty() const { return index == -1; }
+	bool isFull() const { return index == (maxSize - 1); }
+	T pop() {
+		if (this->isEmpty()) throw 2531;
+		return pMem[index--];
+	}
+	void push(T val) {
+		if (this->isFull()) throw 2531;
+		pMem[++index] = val;
+	}
+	T top() { 
+		if (index != -1) return pMem[index];
+		throw 2531;
+	}
+	void clr() { index = -1; }
+};
---
+++ src/test_main.cpp
@@ -0,0 +1,21 @@
+#include"TStack.h"
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	TStack<int> stack(5);
+	cout << stack.isEmpty();
+	stack.push(1);
+	stack.push(2);
+	stack.push(3);
+	stack.push(4);
+	stack.push(5);
+	cout << stack.isFull() << endl;
+	cout << stack.top() << endl;
+	stack.pop();
+	cout << stack.top() << endl;
+	stack.clr();
+	cout << stack.top();
+	
+}
